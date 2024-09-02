65f08d8e22ae68d4ba2bc3cc35c79be36b48e452
david <mrminecraft00@gmail.com>
2024-09-02 11:08:03 +0200

    Add ServiceBank implementation
    Implemented the ServiceBank class that delegates to the net.thenextlvl.economist.api.bank.Bank interface. This includes methods for handling deposits, withdrawals, balance management, and member-related operations. The class ensures proper encapsulation and functionality bridging for the banking service.

---
+++ src/main/java/net/thenextlvl/economist/service/mode/ServiceBank.java
@@ -0,0 +1,76 @@
+package net.thenextlvl.economist.service.mode;
+
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.service.api.economy.bank.Bank;
+import org.bukkit.World;
+import org.jetbrains.annotations.Unmodifiable;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class ServiceBank implements Bank {
+    private final net.thenextlvl.economist.api.bank.Bank bank;
+
+    @Override
+    public BigDecimal deposit(Number amount) {
+        return bank.deposit(amount);
+    }
+
+    @Override
+    public BigDecimal getBalance() {
+        return bank.getBalance();
+    }
+
+    @Override
+    public BigDecimal withdraw(Number amount) {
+        return bank.withdraw(amount);
+    }
+
+    @Override
+    public Optional<World> getWorld() {
+        return bank.getWorld();
+    }
+
+    @Override
+    public UUID getOwner() {
+        return bank.getOwner();
+    }
+
+    @Override
+    public void setBalance(Number balance) {
+        bank.setBalance(balance);
+    }
+
+    @Override
+    public @Unmodifiable Set<UUID> getMembers() {
+        return bank.getMembers();
+    }
+
+    @Override
+    public String getName() {
+        return bank.getName();
+    }
+
+    @Override
+    public boolean addMember(UUID uuid) {
+        return bank.addMember(uuid);
+    }
+
+    @Override
+    public boolean isMember(UUID uuid) {
+        return bank.isMember(uuid);
+    }
+
+    @Override
+    public boolean removeMember(UUID uuid) {
+        return bank.removeMember(uuid);
+    }
+
+    @Override
+    public boolean setOwner(UUID uuid) {
+        return bank.setOwner(uuid);
+    }
+}
