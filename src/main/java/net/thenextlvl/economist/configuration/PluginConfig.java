0d05893c77c6040a11ce75d309363c9ff0ab5b8d
david <mrminecraft00@gmail.com>
2024-09-02 17:30:37 +0200

    Add minimumPayment and balanceAliases to PluginConfig
    Introduce new minimumPayment and balanceAliases fields to PluginConfig to enhance configuration flexibility. This change allows users to specify the minimum payment amount and define multiple aliases for balance, improving customization options for the plugin.

--- src/main/java/net/thenextlvl/economist/configuration/PluginConfig.java
+++ src/main/java/net/thenextlvl/economist/configuration/PluginConfig.java
@@ -4,11 +4,14 @@ import com.google.gson.annotations.SerializedName;
 import org.jetbrains.annotations.Range;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
 public record PluginConfig(
+        @SerializedName("minimum-payment-amount") double minimumPayment,
         @SerializedName("fractional-digits") @Range(from = 0, to = 30) int fractionalDigits,
         @SerializedName("currency-symbol") String currencySymbol,
         @SerializedName("start-balance") BigDecimal startBalance,
-        @SerializedName("storage-type") StorageType storageType
+        @SerializedName("storage-type") StorageType storageType,
+        @SerializedName("balance-aliases") Set<String> balanceAliases
 ) {
 }
