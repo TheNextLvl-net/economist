4837d97ee4c2cbc29c8ae48cd02e7a5191b24e4d
david <mrminecraft00@gmail.com>
2024-09-02 11:07:50 +0200

    Add ServiceAccount class implementation
    Introduced the ServiceAccount class to implement the Account interface. This class includes methods for balance operations such as deposit, withdraw, and set balance, as well as retrieving the account owner and world.

---
+++ src/main/java/net/thenextlvl/economist/service/mode/ServiceAccount.java
@@ -0,0 +1,44 @@
+package net.thenextlvl.economist.service.mode;
+
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.service.api.economy.Account;
+import org.bukkit.World;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class ServiceAccount implements Account {
+    private final net.thenextlvl.economist.api.Account account;
+
+    @Override
+    public BigDecimal deposit(Number amount) {
+        return account.deposit(amount);
+    }
+
+    @Override
+    public BigDecimal getBalance() {
+        return account.getBalance();
+    }
+
+    @Override
+    public BigDecimal withdraw(Number amount) {
+        return account.withdraw(amount);
+    }
+
+    @Override
+    public Optional<World> getWorld() {
+        return account.getWorld();
+    }
+
+    @Override
+    public UUID getOwner() {
+        return account.getOwner();
+    }
+
+    @Override
+    public void setBalance(Number balance) {
+        account.setBalance(balance);
+    }
+}
