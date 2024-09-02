33ddc2ab054b424cc5534223dda128e26da7d75c
david <mrminecraft00@gmail.com>
2024-09-02 15:57:27 +0200

    Refactor SQLiteController constructor to accept EconomistPlugin.
    Switched the constructor parameter from File to EconomistPlugin for better integration with the plugin's data folder. This change helps in dynamically locating the database file within the plugin's directory structure.

--- src/main/java/net/thenextlvl/economist/controller/data/SQLiteController.java
+++ src/main/java/net/thenextlvl/economist/controller/data/SQLiteController.java
@@ -1,11 +1,13 @@
 package net.thenextlvl.economist.controller.data;
 
+import net.thenextlvl.economist.EconomistPlugin;
+
 import java.io.File;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SQLiteController extends SQLController {
-    public SQLiteController(File file) throws SQLException {
-        super(DriverManager.getConnection("jdbc:sqlite:" + file));
+    public SQLiteController(EconomistPlugin plugin) throws SQLException {
+        super(DriverManager.getConnection("jdbc:sqlite:" + new File(plugin.getDataFolder(), "saves.db")), plugin);
     }
 }
