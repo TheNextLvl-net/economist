4f7d6425ca0d2861eb63452f3702096bfe696de9
david <mrminecraft00@gmail.com>
2024-09-05 13:14:57 +0200

    Enhance BankCommand permissions
    Updated BankCommand to include specific permission checks for each sub-command, improving security and control over command access. Also added new sub-commands 'members', 'movements', and 'transfer' to provide more functionality for managing bank operations.

--- src/main/java/net/thenextlvl/economist/command/BankCommand.java
+++ src/main/java/net/thenextlvl/economist/command/BankCommand.java
@@ -13,14 +13,17 @@ public class BankCommand {
     public void register() {
         var command = Commands.literal("bank")
                 .requires(stack -> stack.getSender().hasPermission("economist.bank"))
-                .then(Commands.literal("balance"))
-                .then(Commands.literal("create"))
-                .then(Commands.literal("delete"))
-                .then(Commands.literal("deposit"))
-                .then(Commands.literal("info"))
-                .then(Commands.literal("list"))
-                .then(Commands.literal("top"))
-                .then(Commands.literal("withdraw"))
+                .then(Commands.literal("balance").requires(stack -> stack.getSender().hasPermission("economist.bank.balance")))
+                .then(Commands.literal("create").requires(stack -> stack.getSender().hasPermission("economist.bank.create")))
+                .then(Commands.literal("delete").requires(stack -> stack.getSender().hasPermission("economist.bank.delete")))
+                .then(Commands.literal("deposit").requires(stack -> stack.getSender().hasPermission("economist.bank.deposit")))
+                .then(Commands.literal("info").requires(stack -> stack.getSender().hasPermission("economist.bank.info")))
+                .then(Commands.literal("list").requires(stack -> stack.getSender().hasPermission("economist.bank.list")))
+                .then(Commands.literal("members").requires(stack -> stack.getSender().hasPermission("economist.bank.members")))
+                .then(Commands.literal("movements").requires(stack -> stack.getSender().hasPermission("economist.bank.movements")))
+                .then(Commands.literal("top").requires(stack -> stack.getSender().hasPermission("economist.bank.top")))
+                .then(Commands.literal("transfer").requires(stack -> stack.getSender().hasPermission("economist.bank.transfer")))
+                .then(Commands.literal("withdraw").requires(stack -> stack.getSender().hasPermission("economist.bank.withdraw")))
                 .build();
         plugin.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS.newHandler(event ->
                 event.registrar().register(command, "Manage banks")));
