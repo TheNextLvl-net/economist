6ff0c2f59ae56cc5dd941f90a884f082bfd92405
david <mrminecraft00@gmail.com>
2024-09-08 13:04:21 +0200

    Remove final and add minimum-prune-days to PluginConfig
    Transformed PluginConfig from final to allow inheritance and added the minimum-prune-days configuration option. This change enhances flexibility and introduces a new setting for minimum prune days.

--- src/main/java/net/thenextlvl/economist/configuration/PluginConfig.java
+++ src/main/java/net/thenextlvl/economist/configuration/PluginConfig.java
@@ -8,48 +8,50 @@ import java.util.Set;
 
 @Getter
 @Accessors(fluent = true)
-public final class PluginConfig {
-    private final @SerializedName("storage-type") StorageType storageType = StorageType.SQLite;
+public class PluginConfig {
+    private @SerializedName("storage-type") StorageType storageType = StorageType.SQLite;
 
-    private final @SerializedName("abbreviate-balance") boolean abbreviateBalance = true;
-    private final @SerializedName("scientific-numbers") boolean scientificNumbers = false;
+    private @SerializedName("abbreviate-balance") boolean abbreviateBalance = true;
+    private @SerializedName("scientific-numbers") boolean scientificNumbers = false;
 
-    private final @SerializedName("minimum-payment-amount") double minimumPayment = 0.01;
-    private final @SerializedName("max-loan-amount") double maxLoanAmount = 250;
-    private final @SerializedName("start-balance") double startBalance = 0;
+    private @SerializedName("minimum-payment-amount") double minimumPayment = 0.01;
+    private @SerializedName("max-loan-amount") double maxLoanAmount = 250;
+    private @SerializedName("start-balance") double startBalance = 0;
 
-    private final @SerializedName("balance-aliases") Set<String> balanceAliases = Set.of("bal", "money");
+    private @SerializedName("minimum-prune-days") int minimumPruneDays = 30;
 
-    private final @SerializedName("accounts") AccountConfig accounts = new AccountConfig();
-    private final @SerializedName("balance-top") BalanceTopConfig balanceTop = new BalanceTopConfig();
-    private final @SerializedName("banks") BankConfig banks = new BankConfig();
-    private final @SerializedName("currency") Currency currency = new Currency();
+    private @SerializedName("balance-aliases") Set<String> balanceAliases = Set.of("bal", "money");
+
+    private @SerializedName("accounts") AccountConfig accounts = new AccountConfig();
+    private @SerializedName("balance-top") BalanceTopConfig balanceTop = new BalanceTopConfig();
+    private @SerializedName("banks") BankConfig banks = new BankConfig();
+    private @SerializedName("currency") Currency currency = new Currency();
 
     @Getter
     @Accessors(fluent = true)
-    public static final class AccountConfig {
-        private final @SerializedName("auto-create") boolean autoCreate = true;
-        private final @SerializedName("per-world") boolean perWorld = false;
+    public static class AccountConfig {
+        private @SerializedName("auto-create") boolean autoCreate = true;
+        private @SerializedName("per-world") boolean perWorld = false;
     }
 
     @Getter
     @Accessors(fluent = true)
-    public static final class BalanceTopConfig {
-        private final @SerializedName("entries-per-page") int entriesPerPage = 10;
-        private final @SerializedName("load-unknown-player-names") boolean loadUnknownPlayerNames = false;
+    public static class BalanceTopConfig {
+        private @SerializedName("entries-per-page") int entriesPerPage = 10;
+        private @SerializedName("load-unknown-player-names") boolean loadUnknownPlayerNames = false;
     }
 
     @Getter
     @Accessors(fluent = true)
-    public static final class BankConfig {
-        private final @SerializedName("enabled") boolean enabled = true;
+    public static class BankConfig {
+        private @SerializedName("enabled") boolean enabled = true;
     }
 
     @Getter
     @Accessors(fluent = true)
-    public static final class Currency {
-        private final @SerializedName("currency-symbol") String symbol = "$";
-        private final @SerializedName("minimum-fractional-digits") int minFractionalDigits = 0;
-        private final @SerializedName("maximum-fractional-digits") int maxFractionalDigits = 2;
+    public static class Currency {
+        private @SerializedName("currency-symbol") String symbol = "$";
+        private @SerializedName("minimum-fractional-digits") int minFractionalDigits = 0;
+        private @SerializedName("maximum-fractional-digits") int maxFractionalDigits = 2;
     }
 }
