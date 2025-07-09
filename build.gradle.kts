e5bdefef11cd4bd050fe64b61b2c2af0b2018860
david <github@nonswag.dev>
2025-07-09 20:05:31 +0200

    [ci skip] Added missing Folia loader

--- build.gradle.kts
+++ build.gradle.kts
@@ -312,6 +312,7 @@ modrinth {
     gameVersions.set(versions)
     syncBodyFrom.set(rootProject.file("README.md").readText())
     loaders.add("paper")
+    loaders.add("folia")
     dependencies {
         optional.project("service-io")
     }
