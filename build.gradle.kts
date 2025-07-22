cb65382b71a14ad2fc5833f2344a67af971e7ccf
david <github@nonswag.dev>
2025-07-22 20:52:59 +0200

    Updated `service-io` dependency to `3.0.0-pre6`
    Removed `mavenLocal` from repositories to clean up build configuration and updated `service-io` version to the latest pre-release. This ensures compatibility with new features and fixes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,6 @@ tasks.compileJava {
 }
 
 repositories {
-    mavenLocal()
     mavenCentral()
     maven("https://repo.thenextlvl.net/releases")
     maven("https://repo.papermc.io/repository/maven-public/")
@@ -31,7 +30,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
-    compileOnly("net.thenextlvl.services:service-io:3.0.0-pre5")
+    compileOnly("net.thenextlvl.services:service-io:3.0.0-pre6")
     compileOnly("org.xerial:sqlite-jdbc:3.50.2.0")
     
     implementation("net.thenextlvl.core:i18n:3.2.1")
