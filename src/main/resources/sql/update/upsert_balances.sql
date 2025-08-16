f7e46f215eef52020d18c7311bb7bcfbdae31978
david <github@nonswag.dev>
2025-08-16 21:36:40 +0200

    Refactored balance upsert query
    Simplified SQL by removing unused WITH clause and `accounts` update logic. Enhanced readability and maintained functionality with optimized subquery.

--- src/main/resources/sql/update/upsert_balances.sql
+++ src/main/resources/sql/update/upsert_balances.sql
@@ -1,12 +1,4 @@
-WITH account AS (SELECT id FROM accounts WHERE uuid = ? AND world = ? LIMIT 1)
-
 INSERT
 INTO balances (id, balance, currency)
-SELECT account.id, ?, ?
+SELECT (SELECT id FROM accounts WHERE uuid = ? AND world IS NOT DISTINCT FROM ? LIMIT 1), ?, ?
 ON CONFLICT(id, currency) DO UPDATE SET balance = excluded.balance;
-
-UPDATE accounts
-SET last_update = DEFAULT
-WHERE id = account.id;
--- todo: test if this actually works
--- todo: test if this can be a transaction or if this causes problems with batching
